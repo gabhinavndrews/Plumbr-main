bba5f72a078741c7fe6dd2e76e2a70c2432ef670
Abhinav Andrews Giduturi <gabhinavandrews@gmail.com>
2021-11-22 12:02:40 -0500

    11/22/2021 update plumbr

---
+++ level1.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+favicon = pygame.image.load('pipe.png')  # favicon
+pygame.display.set_icon(favicon)
+background = pygame.image.load('PlumrBGNew.png')
+straight_pipe = pygame.image.load('StraightPipe.png')
+t_pipe = pygame.image.load('TPipe.png')
+plus_pipe = pygame.image.load('PlusPipe.png')
+curve_pipe = pygame.image.load('CurvedPipe.png')
+logo = pygame.image.load('Logo.png')
+
+
+
+
